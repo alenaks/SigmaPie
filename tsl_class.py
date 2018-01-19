4a7ef3e4d87de3b64bf5761c2e9d7a3952889db0
Alëna <loisetoil@gmail.com>
2018-01-19 11:07:41 -0500

    (finally) fixed tier alphabet learner

--- tsl_class.py
+++ tsl_class.py
@@ -26,7 +26,6 @@ class PosTSL(PosSL):
     -- data_sample: the generated data sample;
     -- fsm: the finite state machine that corresponds to the given grammar;
     -- tier: the list of tier symbols.
-    
     """
     
     def __init__(self:PTSL, alphabet:Union[None,list]=None, grammar:Union[None,List[tuple]]=None, k:int=2,
@@ -37,201 +36,89 @@ class PosTSL(PosSL):
         self.tier = tier
 
 
-    def learn(self:TSL) -> None:
-        pass
+    def learn_tier(self:PTSL) -> None:
+        """
+        This function determines which of the symbols used in the language
+        are tier symbols. The learner is taken from Jardine & McMullin (2016).
 
+        Arguments:
+        -- self.
 
-
-
-"""
-
-    def learn(self:TSL) -> None:
-    Function for extracting negative SL grammar and alphabet
-            from the given data.
- 
-        if self.data:
-            self.alphabet = alphabetize(self.data)
-        else:
-            raise IndexError("Language is not provided or empty -- "
-                             "no grammar can be generated.")
+        Results:
+        -- self.tier contains the list of the tier symbols.
+        """
+        
+        self.extract_alphabet()
         self.tier = self.alphabet[:]
 
-        ngrams = self._ngramize_data(self.k, self.data)
-        ngrams_less = self._ngramize_data(self.k-1, self.data)
-        ngrams_more = self._ngramize_data(self.k+1, self.data)
-        
-        for symb in self.tier:
-            if self.__run_tests(symb, ngrams, ngrams_less):
-                print(symb, "must be deleted")
+        if self.tier:
+            ngrams = self.ngramize_data(self.k, self.data)
+            ngrams_less = self.ngramize_data(self.k-1, self.data)
+            ngrams_more = self.ngramize_data(self.k+1, self.data)
 
-    def __run_tests(self:TSL, symb:str, ngrams:list, ngrams_less:list) -> bool:
-     Runs tests to determine whether a symbol is a tier symbol 
+            for symbol in self.tier:
+                if self.test_insert(symbol, ngrams, ngrams_less) and \
+                   self.test_remove(symbol, ngrams, ngrams_more):
+                    self.tier.remove(symbol)
+                   
 
-        test_ins = list(self.__test_insert(symb, ngrams, ngrams_less))
-        if all(test_ins) == True:
+
+    def test_insert(self:PTSL, symbol:str, ngrams:list, ngrams_less:list) -> bool:
+        """
+        Tier presense test #1. For every (n-1)-gram of the type 'xy',
+        there must be an n-gram of the type 'xSy'.
+
+        Arguments:
+        -- self;
+        -- symbol: the symbol that is currently being tested;
+        -- ngrams: the list of ngrams;
+        -- ngrams_less: the list of (n-1)-grams.
+
+        Returns:
+        -- a boolean value depending on whether the symbol passed
+           the test or not.
+        """
+
+        extension = []
+        for small in ngrams_less:
+            for i in range(len(small)):
+                new = small[:i] + (symbol,) + small[i:]
+                if self.good_ngram(new):
+                    extension.append(new)
+                    
+        if set(extension).issubset(set(ngrams)):
             return True
         else:
             return False
+
+
+    def test_remove(self:PTSL, symbol:str, ngrams:list, ngrams_more:list) -> bool:
+        """
+        Tier presense test #2. For every (n+1)-gram of the type 'xSy',
+        there must be an n-gram of the type 'xy'.
+
+        Arguments:
+        -- self;
+        -- symbol: the symbol that is currently being tested;
+        -- ngrams: the list of ngrams;
+        -- ngrams_more: the list of (n+1)-grams.
+
+        Returns:
+        -- a boolean value depending on whether the symbol passed
+           the test or not.
+        """
         
-    def __test_insert(self:TSL, symb:str, ngrams:list, ngrams_less:list) -> Generator[bool, None, None]:
-  
-        
-        for ngram in ngrams_less:
-            new:list = []
-            for ind in range(len(ngram)+1):
-                copy = list(ngram)
-                copy.insert(ind, symb)
-                if good_ngram(tuple(copy)):
-                    new.append(tuple(copy))
-            if set(new).issubset(set(ngrams)):
-                yield True
-            else:
-                yield False
+        extension = []
+        for big in ngrams_more:
+            if symbol in big:
+                for i in range(len(big)):
+                    if big[i] == symbol:
+                        new = big[:i] + big[i+1:]
+                        if self.good_ngram(new):
+                            extension.append(new)
 
-    def __test_remove(self:TSL, symb:str, ngrams:list, ngrams_less:list) -> Generator[bool, None, None]:
-        
-        
-        pass
-        
-
-
-
-
-        ngrams = ngramize_all(obj, n, param)
-
-        # for every symbol in tier, check whether the two tests hold
-        for symb in sigma:
-            if test_insert(obj, n, param, symb, ngrams) and test_remove(obj, n, param, symb, ngrams):
-                # if they do, check whether removing this symbol from the tier will
-                # produce tier sequences that were not found in the input data
-                if erase_test(obj, tier, param, ngrams, symb, n):
-                    # if no, remove the symbol from the tier
-                    tier.remove(symb)
-
-        # generate all possible tier ngrams
-        possib = generate_ngrams(tier, n)
-
-        # extract from the set of possible ngrams those that are allowed
-        # construct the negative tsl grammar
-        neg_gram = possib.difference(ngrams)
-
-        # if the requested grammar is positive, change its polarity
-        if positive == True:
-            pos_gram = change_polarity(neg_gram, tier, n)
-            return pos_gram
-
-        # if negative grammar is expected, return it without modifications              
-        return neg_gram
-
-        
-
-    def erase(data, tier, param):
-         Removes non-tier symbols from the data 
-
-        for item in data:
-            parsed = preprocess(item, param)
-            tier_image = []
-
-            # collects only tier items of the input data
-            for i in parsed:
-                if i in tier:
-                    tier_image.append(i)
-
-            # reassembles the data, and yields it
-            yield "".join(tier_image)
-                    
-
-
-    def erase_test(data, tier, param, ngrams, symb, n):
-         Erasing test: checks that removing the symb from the list
-            of tier symbols will not predict ngrams that were not found
-            in the initial data.
-        
-        
-        newtier = [i for i in tier if i != symb]
-        
-        # create a tier image of the input data, and ngramize it
-        newdata = erase(data, newtier, param)
-        new_ngrams = ngramize_all(newdata, n, param)
-        
-        return new_ngrams.issubset(ngrams)
-
-
-
-    def test_insert(obj, n, param, symb, ngrams):
-         Insertion test: check that for in every (n-1)-gram of the
-            original text it is possible to insert the symbols under
-            consideration in it. If not, it is a tier symbol.
-        
-
-        # get the list of (n-1)-grams
-        test = ngramize_all(obj, n-1, param)
-        
-        # deal with the special case where "<" and ">" are lost
-        if (n-1) == 1:
-            test.add((">",))
-            test.add(("<",))
-
-        # if a symbol can be inserted anywhere, it might be not
-        # a tier symbol, depends on the second test
-        answer = True
-        for i in test:
-            a = set(gen_insert(i, symb))
-            if not (a.issubset(ngrams)):
-                answer = False
+        if set(extension).issubset(set(ngrams)):
+            return True
+        else:
+            return False
                 
-        return answer
-
-
-    def gen_insert(ngram, symb):
-        Auxiliary function for the insertion test 
-        
-        # define start as 0 or index after the last ">"
-        # define end as the last index or index after the first "<"
-        start = 0
-        end = len(ngram)+1
-        if ">" in ngram:
-            ind = [x for x in range(len(ngram)) if ngram[x] == ">"]
-            start = ind[-1]+1
-        if "<" in ngram:
-            ind = [x for x in range(len(ngram)) if ngram[x] == "<"]
-            end = ind[0] + 1
-
-        # generate possible sequences where the symbol is inserted
-        # in the ngram
-        for i in range(start, end):
-            yield ngram[:i] + (symb,) + ngram[i:]
-
-
-
-    def test_remove(obj, n, param, symb, ngrams):
-        Deleteion test: check that for all (n+1)-grams containint
-            the symbol under consideration, this symbol can be removed
-            from there. If not, it is a tier symbol.
-        
-
-        # get the list of (n+1)-grams containing the needed symbol
-        t = ngramize_all(obj, n+1, param)
-        test = set([i for i in t if (symb in i) and (i[n-1] != ">") \
-                    and (i[-n] != "<")])
-
-        # if a symbol can be removed from anywhere, it might be not
-        # a tier symbol, depends on the first test
-        answer = True
-        for i in test:
-            a = set(gen_remove(i, symb))
-            if not (a.issubset(ngrams)):
-                answer = False
-                
-        return answer
-
-
-    def gen_remove(ngram, symb):
-        Auxiliary function for the deletion test
-
-        # generate a set of sequences where each instance of the symbol
-        # is removed from the ngram
-        for i in range(len(ngram)):
-            if ngram[i] == symb:
-                yield ngram[:i] + ngram[i+1:]
-"""
