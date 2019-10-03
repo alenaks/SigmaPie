fdfb2720ac2fcec3c9c8b6224115d95d3a8965a1
alenaks <loisetoil@gmail.com>
2019-10-03 11:37:16 -0400

    added how to run

--- code/main.py
+++ code/main.py
@@ -14,3 +14,14 @@ from sl_class import *
 from tsl_class import *
 from mtsl_class import *
 from sp_class import *
+
+
+print("\nYou successfully loaded SigmaPie. "
+      "Now you can initialize one of the following classes:\n"
+      "\t* strictly piecewise: SP(alphabet, grammar, k, data, polar);\n"
+      "\t* strictly local: SL(alphabet, grammar, k, data, edges, polar);\n"
+      "\t* tier-based strictly local: TSL(alphabet, grammar, k, data, edges,"
+            " polar, tier);\n"
+      "\t* multiple tier-based strictly local: MTSL(alphabet, grammar, k, "
+            "data, edges, polar).\n"
+    )
